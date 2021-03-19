9e77763211d0fd2ba832bbd31f78c259c89436c0
ggabdoll <ggabdoll0327@naver.com>
2021-03-20 00:54:39 +0900

    21.3.20

---
+++ phpjs/4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>JavaScript</h1>
+<script type="text/javascript">
+
+document.write("안녕하세요");
+
+</script>
+<h1>php</h1>
+<?php
+ $name = "egoing"
+ echo "string";
+ ?>
+
+
+</body>
+</html>
