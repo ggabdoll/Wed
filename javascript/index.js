a0072ba1c582f7caa35dc666380716f0a5aab2f0
ggabdoll <ggabdoll0327@naver.com>
2021-03-20 21:12:47 +0900

    21.3.20

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,31 @@
-console.log('Im Fucking Working')
+/*const Like = {food : pizza, sport : soccer, singer: java };
+function lollin() {console.log(Like.food)}
+console.log('Im Fucking Working')*/
+
+/*function sayHello(name, age){
+    return `Hello ${name} you are ${age} years old`;
+}
+
+const greetNicolas = sayHello("Nicolas", 14)
+
+console.log(greetNicolas);*/
+
+/*const title = document.getElementById("title");
+
+title.innerHTML = "Hi! From JS"
+title.style.color = "red";
+document.title = 'I own you now';
+console.log(title);*/
+
+const title = document.querySelector("#title");
+
+/*title.innerHTML = "Hi! From JS"
+title.style.color = "red";
+document.title = 'I own you now';*/
+
+function handleClick(){
+    title.style.color = "blue";
+
+}
+
+title.addEventListener("click",handleClick);
