e29beeb4d838a828756dc42dba7612c8a5d03c9c
ggabdoll <ggabdoll0327@naver.com>
2021-03-22 21:05:20 +0900

    21.3.22

--- javascript/index.js
+++ javascript/index.js
@@ -22,10 +22,31 @@ const title = document.querySelector("#title");
 /*title.innerHTML = "Hi! From JS"
 title.style.color = "red";
 document.title = 'I own you now';*/
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d"
 
 function handleClick(){
-    title.style.color = "blue";
-
+    const currentColor = title.style.color;
+    if(currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    }else{
+        title.style.color = BASE_COLOR;
+    }
 }
 
-title.addEventListener("click",handleClick);
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("mouseenter",handleClick);
+}
+init();
+
+function handleOffline() {
+    console.log("Bye bye")
+}
+
+function handleOnline(){
+    console.log("Welcome back")
+}
+
+window.addEventListener("offline",handleOffline);
+window.addEventListener("online", handleOnline);
