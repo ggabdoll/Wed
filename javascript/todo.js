cb708fc2f926743f028d0cbdd93f879a34cea665
ggabdoll <ggabdoll0327@naver.com>
2021-03-23 20:08:41 +0900

    21.3.23

--- javascript/todo.js
+++ javascript/todo.js
@@ -6,20 +6,34 @@ const TODOS_LS = 'toDos';
 
 const toDos = [];
 
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+}
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "✔";
     const span = document.createElement("span");
+    const newId = toDos.length+1;
+    delBtn.innerText = "✔";
+    delBtn.addEventListener("click",deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
     const toDoObj = {
        text: text,
-       id: toDos.length +1
+       id: newId
     };
     toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -30,9 +44,12 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
+    const loadedToDos = localStorage.getItem(TODOS_LS);
     if(loadToDos !== null){
-
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        });
     } 
 }
 
