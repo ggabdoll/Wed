7f56bda947965ca94ffc6e8aac8de9fad32eed98
ggabdoll <ggabdoll0327@naver.com>
2021-03-24 17:38:10 +0900

    21.3.24

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,18 +1,20 @@
-const clockContainer =document.querySelector(".js-clock"),
-    clockTitle = clockContainer.querySelector("h1"); 
+const clock =document.querySelector(".js-clock .clock__text");
 
 function getTime(){
-    const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours }:${
+    const now = new Date();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    const time = `${hours < 10 ? `0${hours}` : hours }:${
         minutes < 10 ? `0${minutes}` : minutes }`;
+    clock.innerHTML = time;
+    return;
+    
 }
 
 function init(){
     getTime();
     setInterval(getTime,1000);
+    return;
 }
 
 init();
