5fecebe806ed8afc134e180924f6a5c436efb977
ggabdoll <ggabdoll0327@naver.com>
2021-03-24 19:28:06 +0900

    21.3.24

--- javascript/todo.js
+++ javascript/todo.js
@@ -28,9 +28,9 @@ function paintToDo(text){
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length+1;
-    delBtn.innerText = "✔";
-    delBtn.addEventListener("click",deleteToDo);
+    delBtn.innerHTML = "✔";
     delBtn.className = "toDo_btn";
+    delBtn.addEventListener("click",deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
